d930ef6e0175f0fa8448eefd02a839f87581ebcb
Darcy Zhang <darcy_zhang2020@163.com>
2021-03-14 23:46:14 +0800

    Failure... Something need to be polished...

---
+++ Cpp/Acwing/P3.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+#include <iostream>
+
+static int volumn[1001], worth[1001];
+static int solu[1001][1001];
+static int N, V;
+int main()
+{
+    using namespace std;
+    void sele(int m, int n);
+    for (int m = 0; m < 1001; ++m)
+        for (int n = 0; n < 1001; ++n)
+            solu[m][n] = 0;
+    cin >> N >> V;
+    for (int i = 1; i <= N; ++i)
+        scanf("%d%d", &volumn[i], &worth[i]);
+    for (int m = 1; m <= N; ++m)
+        for (int n = 0; n <= V; ++n)
+            sele(m, n);
+    cout << solu[N][V] << endl;
+    return 0;
+}
+
+void sele(int m, int n)
+{
+    int k = 0;
+    int temp = 0;
+    while (0 <= k * volumn[m] && k * volumn[m] <= n)
+    {
+        temp = solu[m - 1][n - k * volumn[m]] + k * worth[m];
+        if (temp > solu[m][n])
+            solu[m][n] = temp;
+        ++k;
+    }
+    return ;
+}
