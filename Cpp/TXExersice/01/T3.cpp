c346e31205bf9789a3ef46e51cfcfb5cfd162520
Darcy Zhang <darcy_zhang2020@163.com>
2021-03-20 19:15:13 +0800

    fixed some bugs

--- Cpp/TXExersice/01/T3.cpp
+++ Cpp/TXExersice/01/T3.cpp
@@ -2,7 +2,7 @@
  * @Author       : Darcy
  * @Date         : 2021-03-19 17:18:20
  * @LastEditors  : Darcy
- * @LastEditTime : 2021-03-19 17:37:24
+ * @LastEditTime : 2021-03-19 18:00:33
  * @FilePath     : \HelloWorld\Cpp\TXExersice\01\T3.cpp
  * @Description  : <(￣︶￣)↗[GO!]
  * https://github.com/Darcy-Zhang
@@ -17,7 +17,7 @@ int main()
     for (int m = 0; m < 7; ++m)
         for (int n = 0; n < 7; ++n)
             cin >> origin[m][n];
-
+    bool t = 1;
     for (int m = 0; m < 7; ++m)
     {
         int maxn = -1;
@@ -32,16 +32,10 @@ int main()
             if (origin[x][maxn] < maxx)
                 continue;
         cout << maxx << endl;
+        t = 0;
         break;
     }
+    if (t)
+        cout << "no" << endl;
     return 0;
 }
-/*
-1 2 3 4 5 6 7
-1 1 1 1 1 1 1
-1 1 1 1 1 1 1
-1 1 1 1 1 1 1
-1 1 1 1 1 1 1
-1 1 1 1 1 1 1
-1 1 1 1 1 1 1
-*/
