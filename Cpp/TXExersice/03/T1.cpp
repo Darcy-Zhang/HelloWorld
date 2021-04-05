a61d18053049ddd75be20bdc3c817f7d69cfb604
Darcy Zhang <darcy_zhang2020@163.com>
2021-04-05 16:27:08 +0800

    在查找一个傻逼的错误上花费了一个小时的时间

---
+++ Cpp/TXExersice/03/T1.cpp
@@ -0,0 +1,92 @@
+/*
+ * @Author       : Darcy
+ * @Date         : 2021-04-04 22:18:31
+ * @LastEditors  : Darcy
+ * @LastEditTime : 2021-04-05 13:16:57
+ * @FilePath     : \HelloWorld\Cpp\TXExersice\03\T1.cpp
+ * @Description  : <(￣︶￣)↗[GO!]
+ * https://github.com/Darcy-Zhang
+ */
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Book
+{
+public:
+   Book(string bname = "MyBook", string bauthor = "Darcy", double bprice = 20.0, unsigned int bnumber = 100);
+   void setname(string name);
+   void display(void);
+   void borrow(void);
+   void restore(void);
+
+private:
+   string bookName;
+   string author;
+   double price;
+   unsigned int number;
+};
+
+Book::Book(string bname, string bauthor, double bprice, unsigned int bnumber)
+{
+   bookName = bname;
+   author = bauthor;
+   price = bprice;
+   number = bnumber;
+}
+
+void Book::setname(string name)
+{
+   bookName = name;
+   return;
+}
+
+void Book::display()
+{
+   cout << "BookName: " << bookName << '\t' << "Author: " << author << '\t' << "Price: " << price << '\t' << "Number: " << number << endl;
+   return;
+}
+
+void Book::borrow()
+{
+   if (number > 0)
+   {
+      --number;
+      cout << bookName << " has been borrowed successfully!" << endl;
+      cout << "BookName: " << bookName << '\t' << "Author: " << author << '\t' << "Price: " << price << '\t' << "Number: " << number << endl;
+   }
+   else
+   {
+      cout << "There are no " << bookName << " in the library" << endl;
+      cout << "BookName: " << bookName << '\t' << "Author: " << author << '\t' << "Price: " << price << '\t' << "Number: " << number << endl;
+   }
+   return;
+}
+
+void Book::restore()
+{
+   ++number;
+   cout << "BookName: " << bookName << '\t' << "Author: " << author << '\t' << "Price: " << price << '\t' << "Number: " << number << endl;
+   return;
+}
+
+int main()
+{
+   Book book;
+   book.setname("C++程序设计");
+   char temp;
+   cout << "click b to borrow, r to restore, s to display and e to exit(b/r/s/e)";
+   while ((temp = getchar()) != 'e')
+   {
+      if (temp == 'b')
+         book.borrow();
+      else if (temp == 'r')
+         book.restore();
+      else if (temp == 's')
+         book.display();
+      else
+         cout << "click b to borrow, r to restore, s to display and e to exit(b/r/s/e)";
+   }
+   return 0;
+}
---
+++ Cpp/TXExersice/03/T2.cpp
@@ -0,0 +1,92 @@
+/*
+ * @Author       : Darcy
+ * @Date         : 2021-04-05 15:15:08
+ * @LastEditors  : Darcy
+ * @LastEditTime : 2021-04-05 16:26:28
+ * @FilePath     : \HelloWorld\Cpp\TXExersice\03\T2.cpp
+ * @Description  : <(￣︶￣)↗[GO!]
+ * https://github.com/Darcy-Zhang
+ */
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Student
+{
+public:
+   int rank;
+   void setinfo(void);
+   void printinfo(void);
+   unsigned int getID(void);
+   string getName(void);
+   unsigned int getGrade(void);
+
+private:
+   unsigned int ID;
+   string name;
+   unsigned int grade;
+};
+
+void Student::setinfo()
+{
+   cin >> ID >> name >> grade;
+   return;
+}
+
+void Student::printinfo()
+{
+   printf("%06d\t", ID);
+   cout << name << '\t' << grade << endl;
+   return;
+}
+
+unsigned int Student::getID()
+{
+   return ID;
+}
+
+string Student::getName()
+{
+   return name;
+}
+
+unsigned int Student::getGrade()
+{
+   return grade;
+}
+
+int main()
+{
+   bool comp(unsigned int a_grade, unsigned int b_grade, unsigned int a_id, unsigned int b_id);
+   Student stu[4];
+   for (int i = 0; i < 4; ++i)
+   {
+      stu[i].setinfo();
+      stu[i].rank = i + 1;
+   }
+   bool t, u;
+   for (int i = 0; i < 4; ++i)
+      for (int n = 0; n < 3; ++n)
+      {
+         t = comp(stu[n].getGrade(), stu[n + 1].getGrade(), stu[n].getID(), stu[n + 1].getID());
+         u = stu[n].rank > stu[n + 1].rank;
+         if (t == u)
+            swap(stu[n].rank, stu[n + 1].rank);
+      }
+   for (int i = 0; i < 4; ++i)
+      for (int n = 0; n < 4; ++n)
+         if (stu[n].rank == (i + 1))
+            stu[n].printinfo();
+   return 0;
+}
+
+bool comp(unsigned int a_grade, unsigned int b_grade, unsigned int a_id, unsigned int b_id)
+{
+   if (a_grade > b_grade)
+      return true;
+   else if (a_grade == b_grade)
+      if (a_id > b_id)
+         return true;
+   return false;
+}
