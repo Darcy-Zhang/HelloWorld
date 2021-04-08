11886172a2804bb0da7fbb853272d3aea932b639
Darcy Zhang <darcy_zhang2020@163.com>
2021-04-08 21:51:43 +0800

    太方便了啊啊啊啊，真的好方便

---
+++ Java/HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+ * @Author       : Darcy
+ * @Date         : 2021-04-08 21:41:43
+ * @LastEditors  : Darcy
+ * @LastEditTime : 2021-04-08 21:50:45
+ * @FilePath     : \HelloWorld\Java\HelloWorld.java
+ * @Description  : <(￣︶￣)↗[GO!]
+ * https://github.com/Darcy-Zhang
+ */
+public class HelloWorld {
+   public static void main(String[] args) {
+      System.out.println("Hello vscode!");
+   }
+}
