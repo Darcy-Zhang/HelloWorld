3179e1609bf9f262b511deed30532dd85fc61f3f
Darcy Zhang <darcy_zhang2020@163.com>
2021-04-08 21:40:53 +0800

    算法书用的是Java。。。。被迫安装Java环境。。

---
+++ Java/Mmp.java
@@ -0,0 +1,14 @@
+/*
+ * @Author       : Darcy
+ * @Date         : 2021-04-08 21:33:24
+ * @LastEditors  : Darcy
+ * @LastEditTime : 2021-04-08 21:36:51
+ * @FilePath     : \HelloWorld\Java\hello.java
+ * @Description  : <(￣︶￣)↗[GO!]
+ * https://github.com/Darcy-Zhang
+ */
+public class Mmp {
+   public static void main() {
+      System.out.println("Hello vscode!");
+   }
+}
