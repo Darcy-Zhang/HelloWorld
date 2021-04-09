338a48ef8bc5a8e8c512b2b5c21ece5213aa47c5
Darcy Zhang <darcy_zhang2020@163.com>
2021-04-09 17:51:49 +0800

    C++课起飞了。。。我写个屁的程序！

---
+++ Cpp/TXExersice/04/T1.cpp
@@ -0,0 +1,61 @@
+/*
+ * @Author       : Darcy
+ * @Date         : 2021-04-09 16:35:38
+ * @LastEditors  : Darcy
+ * @LastEditTime : 2021-04-09 17:06:35
+ * @FilePath     : \HelloWorld\Cpp\TXExersice\04\T1.cpp
+ * @Description  : <(￣︶￣)↗[GO!]
+ * https://github.com/Darcy-Zhang
+ */
+#include <iostream>
+using namespace std;
+
+class ARR
+{
+public:
+   ARR(int x[], int size);
+   void change(void);
+   void show(void);
+
+private:
+   int n;
+   int a[100];
+};
+
+ARR::ARR(int x[], int size)
+{
+   n = size;
+   memcpy(a, x, n * sizeof(int));
+}
+
+void ARR::change()
+{
+   int *p = a;
+   int *q = p + n - 1;
+   while (p != q)
+   {
+      while (*q >= 0 && p < q)
+         q--;
+      while (*p <= 0 && p < q)
+         p++;
+      if (p < q)
+         swap(*p, *q);
+   }
+}
+
+void ARR::show()
+{
+   for (int i = 0; i < n; ++i)
+      cout << a[i] << ' ';
+   cout << endl;
+}
+
+int main()
+{
+   int b[10] = {1, -3, -1, 3, 2, 4, -4, 5, -5, -2};
+   ARR test(b, 10);
+   test.show();
+   test.change();
+   test.show();
+   return 0;
+}
---
+++ Cpp/TXExersice/04/T2.cpp
@@ -0,0 +1,80 @@
+/*
+ * @Author       : Darcy
+ * @Date         : 2021-04-09 17:06:54
+ * @LastEditors  : Darcy
+ * @LastEditTime : 2021-04-09 17:51:30
+ * @FilePath     : \HelloWorld\Cpp\TXExersice\04\T2.cpp
+ * @Description  : <(￣︶￣)↗[GO!]
+ * https://github.com/Darcy-Zhang
+ */
+#include <iostream>
+using namespace std;
+
+class RECT
+{
+public:
+   RECT(double x1, double y1);
+   virtual double area();
+   double peri();
+   virtual int isSquare();
+
+protected:
+   double x, y;
+};
+
+class CUB : public RECT
+{
+public:
+   CUB(double x1, double y1, double z1);
+   double volume();
+   double area();
+   int isSquare();
+
+private:
+   double height;
+};
+
+RECT::RECT(double x1, double y1)
+{
+   x = x1;
+   y = y1;
+}
+
+CUB::CUB(double x1, double y1, double z1) : RECT(x1, y1)
+{
+   height = z1;
+}
+
+double RECT::area()
+{
+   return x * y;
+}
+
+double RECT::peri()
+{
+   return 2 * x + 2 * y;
+}
+
+int RECT::isSquare()
+{
+   return (x == y) ? 1 : 0;
+}
+
+double CUB::volumn()
+{
+   return area() * height;
+}
+
+double CUB::area()
+{
+}
+
+int main()
+{
+   CUB cu;
+   RECT *re = cu;
+   cu.volume();
+   re->area();
+   re->isSquare();
+   return 0;
+}
